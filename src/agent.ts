dd22c4a25ae77e42ad617dc2c9a62bdf8df97d19
Thomas Crochet <thomas@gmail.com>
2025-04-02 15:31:00 +0200

    First commit

---
+++ src/agent.ts
@@ -0,0 +1,43 @@
+import { blModel, blTools, logger } from "@blaxel/sdk";
+import { createTool } from "@mastra/core/tools";
+import { Agent } from "@mastra/core/agent";
+import { z } from "zod";
+
+interface Stream {
+  write: (data: string) => void;
+  end: () => void;
+}
+
+export default async function agent(
+  input: string,
+  stream: Stream
+): Promise<void> {
+  const agent = new Agent({
+    name: "blaxel-agent-mastra",
+    model: await blModel("{{.Model}}").ToMastra(),
+    tools: {
+      ...(await blTools(["blaxel-search"]).ToMastra()),
+      weatherTool: createTool({
+        id: "weatherTool",
+        description: "Get the weather in a specific city",
+        inputSchema: z.object({
+          city: z.string(),
+        }),
+        outputSchema: z.object({
+          weather: z.string(),
+        }),
+        execute: async ({ context }) => {
+          return { weather: `The weather in ${context.city} is sunny` };
+        },
+      }),
+    },
+    instructions: "If the user ask for the weather, use the weather tool.",
+  });
+
+  const response = await agent.stream([{ role: "user", content: input }]);
+
+  for await (const delta of response.textStream) {
+    stream.write(delta);
+  }
+  stream.end();
+}
