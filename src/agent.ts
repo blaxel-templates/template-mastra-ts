5ef68c2a965b536a4be4497de79b95d68f5739ff
drappier-charles <cdrappier@blaxel.ai>
2025-10-30 02:34:39 +0100

    Update sdk

--- src/agent.ts
+++ src/agent.ts
@@ -1,7 +1,5 @@
-import { blModel, blTools } from "@blaxel/mastra";
+import { blModel } from "@blaxel/mastra";
 import { Agent } from "@mastra/core/agent";
-import { createTool } from "@mastra/core/tools";
-import { z } from "zod";
 
 interface Stream {
   write: (data: string) => void;
@@ -14,27 +12,14 @@ export default async function agent(
 ): Promise<void> {
   const agent = new Agent({
     name: "blaxel-agent-mastra",
-    model: await blModel("sandbox-openai"),
-    tools: {
-      ...(await blTools(["blaxel-search"])),
-      weatherTool: createTool({
-        id: "weatherTool",
-        description: "Get the weather in a specific city",
-        inputSchema: z.object({
-          city: z.string(),
-        }),
-        outputSchema: z.object({
-          weather: z.string(),
-        }),
-        execute: async ({ context }) => {
-          return { weather: `The weather in ${context.city} is sunny` };
-        },
-      }),
-    },
     instructions: "If the user ask for the weather, use the weather tool.",
+    model: await blModel("sandbox-openai"),
+    // tools: await blTools(["blaxel-search"]),
   });
 
-  const response = await agent.stream([{ role: "user", content: input }]);
+  const response = await agent.stream(
+    [{ role: "user", content: input }],
+  );
 
   for await (const delta of response.textStream) {
     stream.write(delta);
